8da8318df3aee4b91979b59921ba8de4720b815e
Ari.AlvesSCI <ari.alves@sci.com.br>
2026-03-11 15:40:13 -0300

    fix(backend): ajusta ordem do middleware do cors e adiciona csrf trusted origins.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -129,9 +129,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app']
